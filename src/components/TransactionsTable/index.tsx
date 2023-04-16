587f8a3ee441fb453fc566e04202ce52abccba3f
Pedro <pedrogh.godoy12@gmail.com>
2023-04-15 21:54:38 -0300

    feat: create dashboard components

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,41 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              Desenvolvimento de site
+            </td>
+            <td className="deposit">
+              R$ 12.000,00
+            </td>
+            <td>Desenvolvimento</td>
+            <td>20/12/2022</td>
+          </tr>
+          <tr>
+            <td>
+              Aluguel
+            </td>
+            <td className="withdraw">
+              - R$ 1.000,00
+            </td>
+            <td>Desenvolvimento</td>
+            <td>10/12/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
