c4f6c7eab81a0f0c76e0f110d346b351c27b0209
Pedro <pedrogh.godoy12@gmail.com>
2023-04-18 07:31:50 -0300

    feat: create modal

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/transactions')
-      .then(response => response.json())
-      .then(response => console.log(response))
+    api.get('transactions')
+      .then(response => response)
+      .then(response => console.log(response.data))
   }, []);
 
 
