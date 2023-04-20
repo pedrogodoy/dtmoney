380f75921fe51395ed0730075d76b40705a0351f
Pedro <pedrogh.godoy12@gmail.com>
2023-04-20 10:22:14 -0300

    feat: create income and outcome button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,8 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import { Container, TransactionTypeContainer } from "./styles";
+import closeImg from "../../assets/close.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -14,6 +17,13 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Close modaç" />
+      </button>
       <Container>
         <h2>Cadastrar Transação</h2>
 
@@ -24,6 +34,23 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
           type="number"
           placeholder="Valor"
         />
+
+        <TransactionTypeContainer>
+          <button
+            type="button"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button
+            type="button"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input
           placeholder="Categoria"
         />
